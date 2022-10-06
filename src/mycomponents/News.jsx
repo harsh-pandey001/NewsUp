fcdbd7e2aeab57ac945cb8434f8d042f20ada79c
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-10-06 23:06:12 +0530

    Day-2 commit

--- src/mycomponents/News.jsx
+++ src/mycomponents/News.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
-import Newsitems from './Newsitems'
-
+import React, { Component } from "react";
+import Newsitems from "./Newsitems";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-    the is the News --{`>`}
-      <Newsitems/>
-      <Newsitems/>
-      <Newsitems/>
-      <Newsitems/>
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <Newsitems title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <Newsitems title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <Newsitems title="myTitle" description="mydesc" />
+          </div>
+          
+          
+         
+        </div>
+       
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;

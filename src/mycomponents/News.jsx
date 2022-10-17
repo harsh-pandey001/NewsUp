648818ee30be14de378f37be03a8ab8c2d90a759
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-10-17 12:11:26 +0530

    Day 4 part 2

--- src/mycomponents/News.jsx
+++ src/mycomponents/News.jsx
@@ -2,45 +2,92 @@ import React, { Component } from "react";
 import Newsitems from "./Newsitems";
 
 export class News extends Component {
-  
- constructor() {
+  constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
     };
   }
-  async componentDidMount(){
-     let url = "https://newsapi.org/v2/everything?q=tesla&from=2022-09-16&sortBy=publishedAt&apiKey=72e1bc647ef84ba38af1f3c66e756550";
-     let data = await fetch(url);
-     let parsedData = await data.json()
-     console.log(parsedData);
-     this.setState({articles: parsedData.articles} )
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+    });
   }
+
+  handlePrevClick = async () => {
+    console.log("previous called");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("Next called");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
   render() {
-     return (
+    return (
       <div className="container my-3">
         <h1>NewsUp - Top Headlines</h1>
         <div className="row">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key={element.url}>
-            <Newsitems 
-              title={element.title ? element.title:""}
-              description={element.description ? element.description: ""}
-              imageUrl={element.urlToImage}
-              newsUrl ={element.url}/>
-          </div>
-      
-          }
-          )}
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitems
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
           <div className="my-2">
             <div className="container d-flex justify-content-between">
-            <button type="button" class="btn btn-success" onClick={handleprevClick}>&larr; Previous</button>
-            <button type="button" class="btn btn-warning" onClick={handlenextClick}>Next &rarr;</button>
+              <button
+                disabled={this.state.page <= 1}
+                type="button"
+                className="btn btn-success"
+                onClick={this.handlePrevClick}
+              >
+                &larr; Previous
+              </button>
+              <button
+                type="button"
+                className="btn btn-warning"
+                onClick={this.handleNextClick}
+              >
+                Next &rarr;
+              </button>
             </div>
           </div>
-       
         </div>
       </div>
     );

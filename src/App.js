ab9ef0ee4fde98e470e64b4bb0397093dcbf30ab
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-10-18 01:14:28 +0530

    Day-5 part 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <Navb/>
-    <News/>
+    <News pageSize = {6}/>
     </>
   );
 }
--- src/mycomponents/News.jsx
+++ src/mycomponents/News.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Newsitems from "./Newsitems";
+import Spinner from "./Spinner";
+
 
 export class News extends Component {
   constructor() {
@@ -12,52 +14,57 @@ export class News extends Component {
   }
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=1&pageSize=20";
+      `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=1&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
-      totalArticles: parsedData.totalResults,
+      totalResults: parsedData.totalResults,
+      loading : false
+
     });
   }
 
   handlePrevClick = async () => {
     console.log("previous called");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=${
+    let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
-
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading : false
     });
   };
   handleNextClick = async () => {
     console.log("Next called");
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=${
+    if (!this.state.page + 1 > Math.ceil(this.state.totalResults /this.props.pageSize)) {
+      let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=72e1bc647ef84ba38af1f3c66e756550&page=${
         this.state.page + 1
-      }&pageSize=20`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
       let data = await fetch(url);
       let parsedData = await data.json();
-      console.log(parsedData);
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading : false
       });
     }
+
   };
   render() {
     return (
       <div className="container my-3">
-        <h1>NewsUp - Top Headlines</h1>
+        <h1 className="text-center my-3">NewsUp - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <Newsitems
@@ -80,6 +87,7 @@ export class News extends Component {
                 &larr; Previous
               </button>
               <button
+              disabled ={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
                 type="button"
                 className="btn btn-warning"
                 onClick={this.handleNextClick}
